1f746148e82ed0ce7377edde88c067505bf3564e
skdekedari <pivomoe@mail.ru>
2024-05-20 02:00:58 +0300

    Добавлен Hello3f3f

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,10 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
+            Console.ReadLine();
         }
     }
 }
